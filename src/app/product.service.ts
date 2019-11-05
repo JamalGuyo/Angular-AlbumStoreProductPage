f553a7e77bcb703274e19d351e9b86a424d15957
JamalGuyo <jamalguyo08@gmail.com>
2019-11-05 12:03:32 +0300

    get album list in the product service via http

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
+  private _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) {}
 
-  constructor() { }
-
+  getAlbum(id: number) {
+    return this._http
+      .get(this._albumUrl)
+      .pipe(map((response: Response) => response.json()));
+  }
 }
